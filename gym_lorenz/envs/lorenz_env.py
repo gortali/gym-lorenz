bb5535c0c3a14a42d71bb2a86ae007a6319aa584
Giulio Ortali <g.ortali@tue.nl>
2021-06-04 17:42:00 +0200

    minor modification

--- gym_lorenz/envs/lorenz_env.py
+++ gym_lorenz/envs/lorenz_env.py
@@ -13,25 +13,31 @@ class LorenzEnv(gym.Env):
         self.sigma = 10.0
         self.b= 8.0 / 3.0
 
+        self.t = 0
         self.dt = .05
 
         #action space: a=-1,0,1
-        self.action_space = spaces.Discrete(3)
+        #self.action_space = spaces.Discrete(3)
+        self.action_space = spaces.Box(
+            low=-5,
+            high=5, 
+            shape=(1,),dtype=np.float32
+        )
 
         #observation space: x,y, and z, ubounded
         high = np.array([ np.finfo(np.float32).max,
                           np.finfo(np.float32).max,
                           np.finfo(np.float32).max,
                         ],dtype=np.float32)
-        self.observation_space = spaces.Box(-high, high, dtype=np.float32)
+        self.observation_space = spaces.Box(-high, high,dtype=np.float32)
 
-        self.seed()
+        #self.seed()
 
     #rhs of lorenz system
     def f(self,x,a=0):
         return np.stack([
             self.sigma * (x[1] - x[0]),
-            x[0] * (self.r - x[2]) - x[1] + a,
+            x[0] * (self.r - x[2]) - x[1] + a[0],
             x[0] * x[1] - self.b * x[2]
             ],axis=-1)
 
@@ -52,12 +58,14 @@ class LorenzEnv(gym.Env):
     def step(self, a):
         past = self.x.copy()
         self.x = self.integrate(self.x,a-1)
+        self.t += self.dt
 
-        reward = -float((past[0]*self.x[0])<0) #-(abs(a-1)*0.001)
+        reward = -float((past[0]*self.x[0])<0) #-abs(a[0])*2e-3
         return self.x, reward, False, {}
 
     #initialize to random
     def reset(self):
+        self.t=0
         self.x = 20*np.random.rand(3)
         self.x[0]-=10
         self.x[1]-=10
